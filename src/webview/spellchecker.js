8f7527f9620f03d708bdd655dc72d4e2346cc886
Stefan <stefan@adlk.io>
2017-11-01 22:10:01 +0100

    fix(System): Fix spellchecker initialization

--- src/webview/spellchecker.js
+++ src/webview/spellchecker.js
@@ -1,9 +1,13 @@
 import { SpellCheckHandler, ContextMenuListener, ContextMenuBuilder } from 'electron-spellchecker';
 
 window.spellCheckHandler = new SpellCheckHandler();
-setTimeout(() => {
-  window.spellCheckHandler.attachToInput();
-}, 1000);
+
+window.onload = () => {
+  console.log('init spellchecker');
+  setTimeout(() => {
+    window.spellCheckHandler.attachToInput();
+  }, 1000);
+};
 
 // TODO: should we set the language to user settings?
 // window.spellCheckHandler.switchLanguage('en-US');
